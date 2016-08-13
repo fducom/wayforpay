8b1ff720b153bfe7e329e8b09b803286a2c47ae8
Dima Fedorchuk <fducom@gmail.com>
2016-08-13 15:02:03 +0300

    success

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 var WayForPay = require('wayforpay');
+
 var wayforpay = new WayForPay(merchant_account, merchant_password);
-var generatePurchaseUrl = wayforpay.generatePurchaseUrl({
-'action'         : 'pay',
-'amount'         : '1',
-'currency'       : 'USD',
-'description'    : 'description text',
-'order_id'       : 'order_id_1'
-});
+
+var params = {
+    'merchantDomainName': 'app.wepster.com',
+    'merchantTransactionSecureType': 'AUTO',
+    'serviceUrl': 'http://app.wepster.com/wfp/return',
+    'orderReference': 'orderid001',
+    'orderDate': '14898322',
+    'amount': '1.00',
+    'currency': 'USD',
+    'productName': 'product name',
+    'productPrice': orderDatasGet.price,
+    'productCount': "1",
+    'language': "ru",
+};
+
+var generatePurchaseUrl = wayforpay.generatePurchaseUrl(params);
+
--- wayforpay.js
+++ wayforpay.js
@@ -13,12 +13,9 @@
 
  */
 var request = require('request');
-var crypto  = require('crypto');
+var crypto = require('crypto');
 var _ = require('lodash');
-var http_build_query = require('locutus/php/url/http_build_query'); //npm install locutus
-//var http_build_query = require('qhttp/http_build_query'); //  var qhttp = require('qhttp'); var http_build_query = require('qhttp/http_build_query'); //  var qhttp = require('qhttp');
-
-
+var utf8 = require('utf8');
 
 /**
  * Constructor.
@@ -28,109 +25,320 @@ var http_build_query = require('locutus/php/url/http_build_query'); //npm instal
  *
  * @throws InvalidArgumentException
  */
-module.exports = function(merchant_account, merchant_password) {
+module.exports = function (merchant_account, merchant_password) {
 
     // API host
 
     const PURCHASE_URL = 'https://secure.wayforpay.com/pay';
     const API_URL = 'https://api.wayforpay.com/api';
-    const MODE_PURCHASE = 'PURCHASE';
+    const WIDGET_URL = 'https://secure.wayforpay.com/server/pay-widget.js';
+    const FIELDS_DELIMITER = ';';
     const API_VERSION = 1;
+    const DEFAULT_CHARSET = 'utf8';
+    const MODE_PURCHASE = 'PURCHASE';
+    const MODE_SETTLE = 'SETTLE';
+    const MODE_CHARGE = 'CHARGE';
+    const MODE_REFUND = 'REFUND';
+    const MODE_CHECK_STATUS = 'CHECK_STATUS';
+    const MODE_P2P_CREDIT = 'P2P_CREDIT';
+    const MODE_CREATE_INVOICE = 'CREATE_INVOICE';
+    const MODE_P2_PHONE = 'P2_PHONE';
+    const COMPLETE_3DS = 'COMPLETE_3DS';
+    this._action;
+    this._fields;
+    this._charset;
+
     /**
      * Call API
      *
-     * @param Object $params
-     * @param function $callback
+     * @param Object fields
      *
      * @return Object
      */
-    this.api = function( params, callback, callbackerr){
+    this._query = function () {
 
+        //var options = {
+        //    url: API_URL,
+        //    headers: {
+        //        'content-type': 'application/json'
+        //    }
+        //};
+        console.log('_query');
 
-        var data = JSON.stringify(params);
-        request.post(API_URL , data, function (error, response, body) {
+        let data = JSON.stringify(this._fields);
+        request.put({
+                url: API_URL,
+                body: data,
+                headers: {'Content-Type': 'application/json'}
+            }, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    callback( JSON.parse(body) )
-                }else{
-                    callbackerr(error, response);
+                    return 'ok';
+                } else {
+                    return body;
                 }
             }
         );
     };
 
+    /**
+     * Return signature hash
+     *
+     * @param action
+     * @param fields
+     * @return mixed
+     */
+    this.createSignature = function (action, fields) {
 
-    this._prepare = function(action, params)
-    {
+        this._prepare(action, fields);
+        return this._buildSignature();
+    };
+
+    this._prepare = function (action, fields) {
+        console.log('_prepare');
+
         this._action = action;
-        if (!params || 0 === params.length) {
+        if (_.isEmpty(fields)) {
             throw new Error('Arguments must be not empty');
         }
-        this._params = params;
-        this._params.transactionType = action;
-        this._params.merchantAccount = merchant_account;
-        this._params.merchantSignature = this._buildSignature(params);
+        this._fields = fields;
 
-        if(this._action !== MODE_PURCHASE) this._params.apiVersion = API_VERSION;
-        this._params = this._checkFields(this._params);
-    }
+        this._fields.transactionType = action;
+        this._fields.merchantAccount = merchant_account;
+        this._fields.merchantSignature = this._buildSignature();
 
-    this.generatePurchaseUrl = function(params)
-    {
-        this._prepare(MODE_PURCHASE, params);
-        //dd($this->_params);
+        if (this._action !== MODE_PURCHASE) this._fields.apiVersion = API_VERSION;
+        this._checkFields();
+    };
 
-        // let arrParam = Object.values(params);
+    /**
+     * MODE_SETTLE
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.settle = function (fields) {
+        this._prepare(MODE_SETTLE, fields);
+        return this._query();
+    };
 
+    /**
+     * MODE_CHARGE
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.charge = function (fields) {
+        this._prepare(MODE_CHARGE, fields);
 
+        return this._query();
+    };
 
-        //console.log(serialize({foo: "hi there", bar: "100%" }));
+    /**
+     * MODE_REFUND
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.refund = function (fields) {
+        this._prepare(MODE_REFUND, fields);
+        return this._query();
+    };
 
-        return PURCHASE_URL + '/get?' + serialize(params);
-    }
+    /**
+     * MODE_CHECK_STATUS
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.checkStatus = function (fields) {
+        this._prepare(MODE_CHECK_STATUS, fields);
+        return this._query();
+    };
 
-    var serialize = function(obj) {
-        var str = [];
-        for(var p in obj)
-            if (obj.hasOwnProperty(p)) {
-                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    /**
+     * COMPLETE_3DS
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.complete3ds = function (fields) {
+        this._prepare(COMPLETE_3DS, fields);
+        return this._query();
+    };
+
+    /**
+     * MODE_P2P_CREDIT
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.account2card = function (fields) {
+        this._prepare(MODE_P2P_CREDIT, fields);
+        return this._query();
+    };
+
+    /**
+     * MODE_P2P_CREDIT
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.createInvoice = function (fields) {
+        this._prepare(MODE_CREATE_INVOICE, fields);
+        return this._query();
+    };
+
+    /**
+     * MODE_P2P_CREDIT
+     *
+     * @param fields
+     * @return mixed
+     */
+    this.account2phone = function (fields) {
+        this._prepare(MODE_P2_PHONE, fields);
+        return this._query();
+    };
+    /**
+     * MODE_PURCHASE
+     * Generate html form
+     *
+     * @param fields
+     * @return string
+     */
+    this.buildForm = function (fields) {
+        this._prepare(MODE_PURCHASE, fields);
+        var form = '<form method="POST" action="' + PURCHASE_URL + '" accept-charset="utf-8">';
+        _.each(fields, function (value, key) {
+            if (_.isArray(key)) {
+                _.each(key, function (fild) {
+                    form += '<input type="hidden" name="' + key + '[]" value="' + fild + '" />';
+                });
+            } else {
+                form += '<input type="hidden" name="' + key + '" value="' + value + '" />';
             }
-        return str.join("&");
-    }
+        });
+        form += '<input type="submit" value="Submit purchase form"></form>';
+        return form;
+    };
+    /**
+     * MODE_PURCHASE
+     * If GET redirect is used to redirect to purchase form, i.e.
+     * https://secure.wayforpay.com/pay/get?merchantAccount=test_merch_n1&merchantDomainName=domain.ua&merchantSignature=c6d08855677ec6beca68e292b2c3c6ae&orderReference=RG3656-1430373125&orderDate=1430373125&amount=0.16&currency=UAH&productName=Saturn%20BUE%201.2&productPrice=0.16&productCount=1&language=RU
+     *
+     * @param fields
+     * @return string
+     */
+    this.generatePurchaseUrl = function (fields) {
+        this._prepare(MODE_PURCHASE, fields);
+        return PURCHASE_URL + '/get?' + serialize(fields);
+    };
 
+
+    this._getFieldsNameForSignature = function () {
+        console.log('_getFieldsNameForSignature');
+
+        const purchaseFieldsAlias = [
+            'merchantAccount',
+            'merchantDomainName',
+            'orderReference',
+            'orderDate',
+            'amount',
+            'currency',
+            'productName',
+            'productCount',
+            'productPrice'
+        ];
+        switch (this._action) {
+            case 'COMPLETE_3DS':
+                return [
+                    'transactionType',
+                    'authorization_ticket',
+                    'd3ds_pares'
+                ];
+                break;
+
+            case 'PURCHASE':
+                return purchaseFieldsAlias;
+                break;
+            case 'REFUND':
+                return [
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency'
+                ];
+            case 'CHECK_STATUS':
+                return [
+                    'merchantAccount',
+                    'orderReference'
+                ];
+                break;
+            case 'CHARGE':
+                return purchaseFieldsAlias;
+                break;
+            case 'SETTLE':
+                return [
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency'
+                ];
+                break;
+            case MODE_P2P_CREDIT:
+                return [
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency',
+                    'cardBeneficiary',
+                    'rec2Token',
+                ];
+                break;
+            case MODE_CREATE_INVOICE:
+                return purchaseFieldsAlias;
+                break;
+            case MODE_P2_PHONE:
+                return [
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency',
+                    'phone',
+                ];
+                break;
+            default:
+                throw new Error('Unknown transaction type: ' + this._action);
+        }
+    };
 
 
     /**
      * _checkFields
      *
-     * @param Object $params
+     * @param Object fields
      *
      * @return status
      *
      * @throws InvalidArgumentException
      */
-    this._checkFields = function(params){
+    this._checkFields = function () {
+        console.log('_checkFields');
+        var required = this._getRequiredFields;
+        let error = [];
+        var parameters = this._fields;
+        _(required).forEach(function (item) {
+            if (array_key_exists(item, parameters)) {
+                if (!(parameters[item])) {
+                    error.push(item);
+                }
+            } else {
+                error.push(item);
+            }
+        });
 
-
-        if(!params.merchantAccount)
-            throw new Error('version is null');
-        if(!params.merchantDomainName)
-            throw new Error('amount is null');
-        if(!params.orderReference)
-            throw new Error('currency is null');
-        if(!params.orderDate)
-            throw new Error('description is null');
-        if(!params.amount)
-            throw new Error('description is null');
-        if(!params.currency)
-            throw new Error('description is null');
-        if(!params.productName)
-            throw new Error('description is null');
-        if(!params.productCount)
-            throw new Error('description is null');
-        if(!params.productPrice)
-            throw new Error('description is null');
-        //if(!params.merchantSignature)
-        //    throw new Error('description is null');
+        if (!_.isEmpty(error)) { //!_.isEmpty
+            throw new Error('Missed required field(s): ' + JSON.stringify(error));
+        }
 
         return true;
 
@@ -140,23 +348,199 @@ module.exports = function(merchant_account, merchant_password) {
     /**
      * _buildSignature
      *
-     * @param Object params
+     * @param Object fields
      *
      * @return string
      */
-    this._buildSignature = function(params){
+    this._buildSignature = function () {
+        console.log('_buildSignature');
 
-        let arrParam = _.values(params);
+        let signFields = this._getFieldsNameForSignature();
+        let data = [];
+        let error = [];
+        var parameters = this._fields;
 
-        const secret = arrParam.join(';');
-        var buffer = new TextEncoder("utf-8").encode(secret);
-        const hash = crypto.createHmac('md5', buffer)
-            .update(merchant_password)
+        _(signFields).forEach(function (item) {
+            if (array_key_exists(item, parameters)) {
+                let value = parameters[item];
+                if (_.isArray(value)) {
+                    let arrParam = _.values(value);
+                    const str = arrParam.join(FIELDS_DELIMITER);
+                    data.push(str + "");
+                } else {
+                    data.push(value + "");
+                }
+            } else {
+                error.push(item);
+            }
+        });
+
+        if (!_.isEmpty(error)) {
+            throw new Error('Missed signature field(s): ' + JSON.stringify(error));
+        }
+        let arrParam = _.values(data);
+        const secret = arrParam.join(FIELDS_DELIMITER);
+        var buffer = utf8.encode(secret);
+
+        const hash = crypto.createHmac('md5', merchant_password)
+            .update(buffer)
             .digest('hex');
 
         return hash;
 
     };
+
+
+    this._getRequiredFields = function () {
+        switch (this._action) {
+            case 'PURCHASE':
+                return [
+                    'merchantAccount',
+                    'merchantDomainName',
+                    'merchantTransactionSecureType',
+                    'orderReference',
+                    'orderDate',
+                    'amount',
+                    'currency',
+                    'productName',
+                    'productCount',
+                    'productPrice'
+                ];
+            case 'SETTLE':
+                return [
+                    'transactionType',
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency',
+                    'apiVersion'
+                ];
+            case 'CHARGE':
+                var required = [
+                    'transactionType',
+                    'merchantAccount',
+                    'merchantDomainName',
+                    'orderReference',
+                    'apiVersion',
+                    'orderDate',
+                    'amount',
+                    'currency',
+                    'productName',
+                    'productCount',
+                    'productPrice'
+                ];
+                var additional = (this._fields['recToken']) ?
+                    ['recToken'] :
+                    ['card', 'expMonth', 'expYear', 'cardCvv', 'cardHolder'];
+                return required.concat(additional).unique();
+            case 'REFUND':
+                return [
+                    'transactionType',
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency',
+                    'comment',
+                    'apiVersion'
+                ];
+            case 'CHECK_STATUS':
+                return [
+                    'transactionType',
+                    'merchantAccount',
+                    'orderReference',
+                    'apiVersion'
+                ];
+            case 'COMPLETE_3DS':
+                return [
+                    'transactionType',
+                    'authorization_ticket',
+                    'd3ds_pares'
+                ];
+            case MODE_P2P_CREDIT:
+                return [
+                    'transactionType',
+                    'merchantAccount',
+                    'orderReference',
+                    'amount',
+                    'currency',
+                    'cardBeneficiary',
+                    'merchantSignature'
+                ];
+            case MODE_CREATE_INVOICE:
+                return [
+                    'transactionType',
+                    'merchantAccount',
+                    'merchantDomainName',
+                    'orderReference',
+                    'amount',
+                    'currency',
+                    'productName',
+                    'productCount',
+                    'productPrice'
+                ];
+            case MODE_P2_PHONE:
+                return [
+                    'merchantAccount',
+                    'orderReference',
+                    'orderDate',
+                    'currency',
+                    'amount',
+                    'phone',
+                    'apiVersion'
+                ];
+                break;
+            default:
+                throw new Error('Unknown transaction type');
+        }
+    };
+
+
+    this.buildWidgetButton = function (fields, callback = null) {
+        this._prepare(MODE_PURCHASE, fields);
+        let button = '<script id="widget-wfp-script" language="javascript" type="text/javascript" src="' + WIDGET_URL + '"></script>';
+        button += '<script type="text/javascript">';
+        button += 'var wayforpay = new Wayforpay();';
+        button += 'var pay = function () {';
+        button += '    wayforpay.run(' + JSON.stringify(this._fields) + ');';
+        button += '}';
+        button += 'window.addEventListener("message", ' + (callback ? callback : "receiveMessage") + ');';
+        button += 'function receiveMessage(event)';
+        button += '{';
+        button += '    if(';
+        button += '        event.data == "WfpWidgetEventClose" ||      //при закрытии виджета пользователем';
+        button += '        event.data == "WfpWidgetEventApproved" ||   //при успешном завершении операции';
+        button += '        event.data == "WfpWidgetEventDeclined" ||   //при неуспешном завершении';
+        button += '        event.data == "WfpWidgetEventPending")      // транзакция на обработке';
+        button += '    {';
+        button += '        console.log(event.data);';
+        button += '    }';
+        button += '}';
+        button += '</script>';
+        button += '<button type="button" onclick="pay();">Оплатить</button>';
+        return button;
+    };
+
+
+    ////refact
+
+    var serialize = function (obj) {
+        var str = [];
+        for (var p in obj)
+            if (obj.hasOwnProperty(p)) {
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            }
+        return str.join("&");
+    };
+
+    var array_key_exists = function (key, search) {
+
+        if (!search || (search.constructor !== Array && search.constructor !== Object)) {
+            return false;
+        }
+
+        return search[key] !== undefined;
+    };
+    ////refactend
 
     return this;
 };
